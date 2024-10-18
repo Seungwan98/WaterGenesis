7c0469e517d36ada4ccfb817e9ab3afe6ea3a1ee
Seungwan98 <sinna3210@gmail.com>
2024-10-18 10:11:35 +0900

    Initial Commit

---
+++ WaterGenesis/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WaterGenesis
+//
+//  Created by 이노프렌즈 on 10/18/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
